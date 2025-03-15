ba6780627bad32fce5f8682794f932891c8e63c1
Ram Kaucha <z5367751@ad.unsw.edu.au>
2025-03-15 17:13:07 +1100

    landing page, nav

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -3,15 +3,19 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import CreateAccountButton from "@/components/CreateAccountButton";
+import { ArrowRight } from "lucide-react";
+import { ModeToggle } from "./NodeToggle";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Navigation() {
   const [scrolled, setScrolled] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 10) {
-        setScrolled(true);x
+        setScrolled(true);
       } else {
         setScrolled(false);
       }
@@ -28,26 +32,25 @@ export default function Navigation() {
   return (
     <motion.header
       className={`fixed w-full top-0 z-50 transition-all duration-300 ${
-        scrolled ? "bg-gray-800/80 backdrop-blur-sm shadow-md" : "bg-transparent"
+        scrolled ? "bg-white/80 dark:bg-gray-800/80 backdrop-blur-sm shadow-md" : "bg-transparent"
       }`}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
-          <div className="flex items-center">
-            <Link href="/" className="font-bold text-xl text-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mb-4">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center justify-center">
+            <Link href="/" className="font-bold text-xl dark:text-white text-black">
               Solve.
             </Link>
           </div>
-          <nav className="flex space-x-6">
-            <Link href="/login" className="text-gray-300 hover:text-white transition-colors" >
-              Create an Account !
-            </Link>
-            <Link href="/register" className="text-gray-300 hover:text-white transition-colors">
-              Sign In
-            </Link>
+          <nav className="flex space-x-6 items-center mt-3">
+            <Button onClick={() => router.push('/login')} variant="outline" className="text-gray-900 dark:text-gray-300 transition-colors flex flex-row items-center space-x-2 border-1 rounded-md py-2 px-3" >
+              <span>Sign In!</span>
+              <ArrowRight className="w-4 h-4" />
+            </Button>
+            <ModeToggle/>
           </nav>
         </div>
       </div>
