8f8443ae3368448469d53b85aceda9d532e7dd11
Shuimu <linlang1014@gmail.com>
2025-03-16 13:49:06 +1100

    change the register function in user service

--- backend/app/service/user_service.py
+++ backend/app/service/user_service.py
@@ -15,23 +15,23 @@ class UserService:
     def __init__(self):
         self.user_model = User()
 
-    def register(user: User):
+    def register(name:str, password:str, email:str):
         # test if the user has been exixted
-        existing_user = User.query.filter_by(email=user.email).first()
+        existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             return jsonify({"message:": "the user has been existed"}), 404
 
-        if user is None:
-            return jsonify({"message:": "invalid user info input"})
-        if user.user_name is None:
+        # if user is None:
+        #     return jsonify({"message:": "invalid user info input"})
+        if name is None:
             return jsonify({"message:": "you must set your user name"})
-        if user.password is None:
+        if password is None:
             return jsonify({"message:": "you must set your password"})
-        if user.email in None:
+        if email in None:
             return jsonify({"message:": "you must set your email"})
 
-        hash_pw = PassUtil.encrypt_password(user.password)
-        new_user = User(userName=user.user_name, password=hash_pw, email=user.email, credit=0, isNew=True,
+        hash_pw = PassUtil.encrypt_password(password)
+        new_user = User(userName=name, password=hash_pw, email=email, credit=0, isNew=True,
                         isDelete=False)
         db.session.add(new_user)
         db.session.commit()
